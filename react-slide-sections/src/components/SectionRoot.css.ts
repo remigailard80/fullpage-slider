2cf82ab178d0cee6cff7af94b1e7cf981487611c
nash <nash@daangn.com>
2022-09-06 00:51:04 +0900

    refactor to wheel event listening logic

--- react-slide-sections/src/components/SectionRoot.css.ts
+++ react-slide-sections/src/components/SectionRoot.css.ts
@@ -3,9 +3,26 @@ import { style } from "@vanilla-extract/css";
 export const SectionRootStyle = style({
   width: "100%",
   height: "100vh",
+  position: "relative",
+  overflow: "hidden",
 });
 
 export const SectionRootItemWrapperStyle = style({
   width: "100%",
   height: "100vh",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  transform: `translate3d(0px, 0px, 0px)`,
+  transition: "all ease 1s",
+});
+
+export const SectionRootInActiveItemWrapperStyle = style({
+  width: "100%",
+  height: "100vh",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  transform: `translate3d(0px, -100%, 0px)`,
+  transition: "all ease 1s",
 });
--- react-slide-sections/src/components/SectionRoot.tsx
+++ react-slide-sections/src/components/SectionRoot.tsx
@@ -1,123 +1,83 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 
 import {
   SectionRootStyle,
   SectionRootItemWrapperStyle,
+  SectionRootInActiveItemWrapperStyle,
 } from "./SectionRoot.css";
+import { reverseArrayIndex } from "../utils/Array";
 
 interface SectionRootProps {
+  threshold?: number;
   children: React.ReactNode[];
 }
 
 let scrollThrottle;
 
-export const SectionRoot: React.FC<SectionRootProps> = ({ children }) => {
-  const rootElementRef = useRef<HTMLDivElement>(null);
+export const SectionRoot: React.FC<SectionRootProps> = ({
+  threshold,
+  children,
+}) => {
+  const scrollThreshold = threshold || 0;
   const [activeIdx, setActiveIdx] = useState<number>(0);
-  const [lastScrollY, setLastScrollY] = useState<number>(
-    window.pageYOffset || window.scrollY
-  );
-  const [scrollable, setScrollable] = useState<boolean>(true);
-
-  const detectScrollDirection = useCallback(
-    (e) => {
-      const currentWindow = e.currentTarget;
-      const isScrollUp = lastScrollY > currentWindow.scrollTop;
-      const isScrollDown = lastScrollY < currentWindow.scrollTop;
-
-      if (isScrollDown) {
-        return "SCROLL_DOWN";
-      }
-      if (isScrollUp) {
-        return "SCROLL_UP";
-      }
-      return;
-    },
-    [lastScrollY]
+  const activeIndexes = useMemo(
+    () => Array.from({ length: children.length }).map((_, index) => index),
+    [children]
   );
 
-  const handleScrollThrottle = () => {
-    // TODO
+  const handleScrollThrottle = (timeout: number) => {
+    if (scrollThrottle) return;
     scrollThrottle = true;
-    setTimeout(() => {
+    scrollThrottle = setTimeout(() => {
       scrollThrottle = false;
-    }, 1000);
+    }, timeout);
   };
 
-  const setScreenNotScrollableDuringScrollThrottle = useCallback(() => {
-    setScrollable(false);
-    setTimeout(() => {
-      setScrollable(true);
-    }, 1200);
-  }, [scrollable]);
-
-  const windowScrollTo = (position: number) => {
-    rootElementRef.current.scrollTo({
-      left: 0,
-      top: position,
-      behavior: "smooth",
-    });
-    return;
-  };
-
-  const handleScroll = useCallback(
+  const handleWheelEvent = useCallback(
     (e) => {
-      if (scrollThrottle) {
-        // scroll Action Processing Throttle
+      const isDeltaYThresholdOver = Math.abs(e.deltaY) >= scrollThreshold;
+      const isScrollUp = e.deltaY > 0 && isDeltaYThresholdOver;
+      const isScrollDown = e.deltaY < 0 && isDeltaYThresholdOver;
+
+      if (scrollThrottle) return;
+
+      if (isScrollUp) {
+        handleScrollThrottle(1000);
+        setActiveIdx((prev) => prev + 1);
         return;
       }
-      // if not processing scroll Action
-      handleScrollThrottle();
-
-      // Detect Scroll Direction
-      const scrollDirection = detectScrollDirection(e);
-      if (scrollDirection === "SCROLL_DOWN") {
-        const nextActiveIdx =
-          activeIdx < children.length - 1 ? activeIdx + 1 : children.length - 1;
-
-        setActiveIdx(nextActiveIdx);
-        setLastScrollY(nextActiveIdx * rootElementRef.current.clientHeight);
-        setScreenNotScrollableDuringScrollThrottle();
-
-        windowScrollTo(nextActiveIdx * rootElementRef.current.clientHeight);
-        return;
-      }
-      if (scrollDirection === "SCROLL_UP") {
-        const nextActiveIdx = activeIdx > 0 ? activeIdx - 1 : 0;
-
-        setActiveIdx(nextActiveIdx);
-        setLastScrollY(nextActiveIdx * rootElementRef.current.clientHeight);
-        setScreenNotScrollableDuringScrollThrottle();
-
-        windowScrollTo(nextActiveIdx * rootElementRef.current.clientHeight);
+      if (isScrollDown) {
+        handleScrollThrottle(1000);
+        setActiveIdx((prev) => prev - 1);
         return;
       }
     },
-    [
-      rootElementRef,
-      detectScrollDirection,
-      setScreenNotScrollableDuringScrollThrottle,
-      activeIdx,
-      children,
-    ]
+    [scrollThreshold]
   );
 
   useEffect(() => {
-    rootElementRef.current.addEventListener("scroll", handleScroll);
-    return () => {
-      rootElementRef.current.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
+    window.addEventListener("wheel", handleWheelEvent);
+    return () => window.removeEventListener("wheel", handleWheelEvent);
+  }, [handleWheelEvent]);
+
+  console.log(activeIdx);
 
   return (
-    <main
-      ref={rootElementRef}
-      className={SectionRootStyle}
-      style={{ overflow: scrollable ? "auto" : "hidden" }}
-    >
+    <main className={SectionRootStyle}>
       {React.Children.map(children, (child, idx) => {
+        const isActiveSection =
+          reverseArrayIndex(activeIndexes, activeIdx) <= idx;
         return (
-          <section className={SectionRootItemWrapperStyle}>{child}</section>
+          <section
+            className={
+              isActiveSection
+                ? SectionRootItemWrapperStyle
+                : SectionRootInActiveItemWrapperStyle
+            }
+            style={{ zIndex: 100 - idx }}
+          >
+            {child}
+          </section>
         );
       })}
     </main>
