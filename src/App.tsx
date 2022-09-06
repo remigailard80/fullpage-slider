5bca16c16870a3bdc3d5d0626932f90fbded2850
nash <nash@daangn.com>
2022-09-07 00:18:13 +0900

    add loopingScroll feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { SectionRoot } from "react-slide-sections";
 
 function App() {
   return (
-    <SectionRoot threshold={20}>
+    <SectionRoot threshold={20} loopingScroll={false}>
       <div style={{ backgroundColor: "red", height: "100%" }}> Paper1 </div>
       <div style={{ backgroundColor: "blue", height: "100%" }}> Paper2 </div>
       <div style={{ backgroundColor: "green", height: "100%" }}> Paper3 </div>
